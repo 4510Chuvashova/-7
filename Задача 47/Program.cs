ff8db927f4c53a5ace4bcdfa60a70d6b601c1889
Marina <c.ms14@hotmail.com>
2023-07-06 12:43:54 +1000

    добавила первую задачу

---
+++ Задача 47/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine ("введите количество строк");
+int linesVol = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("введите количество столбцов");
+int columnsVol = Convert.ToInt32 (Console.ReadLine());
+double [,] numbers = new double [linesVol, columnsVol];
+FillArrayRandomNumbers (numbers);
+PrintArray (numbers);
+void FillArrayRandomNumbers (double [,] array)
+{
+    for (int i = 0; i < array.GetLength (0); i ++)
+    {
+        for (int k = 0; k < array.GetLength (1); k++)
+        {
+            array [i,k] = Convert.ToDouble (new Random(). Next (-100,100)) / 10;
+        }
+    }
+}
+void PrintArray (double [,] array)
+{
+    for (int i = 0; i < array.GetLength (0); i++)
+    {
+        Console.Write ("[ ");
+        for (int k = 0; k < array.GetLength (1); k++)
+    {
+        Console.Write (array [i, k] + " ");
+    }
+Console.Write ("]");
+Console.WriteLine ("");
+    }
+}
